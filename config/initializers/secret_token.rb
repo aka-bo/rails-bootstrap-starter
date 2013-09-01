765e2dc8c0a040c71916d064425a4501875e9240
Bo Thompson <bo.thompson@gmail.com>
2013-09-01 14:21:46 -0700

    Default Rails 4.0 site up and running.
    Notable dependencies
    - therubyracer via v8
    - mysql2
    - rspec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsBootstrapTemplate::Application.config.secret_key_base = 'ca63297b46c90fe35b0fb6811088a2ec383104551ede3c809d72a0034680f5f276be8fdc03e17b0848b0e3279cd063791e54243ebc376282d61e166232825abd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailsBootstrapTemplate::Application.config.session_store :cookie_store, key: '_rails-bootstrap-template_session'
